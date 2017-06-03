ed1b84e5c72fbde93f51c6eec0bda0f84e7a48dd
tmgree <tmgree@outlook.com>
2017-06-03 16:14:00 +0100

    added prime factors function

--- Proper_Fractions/lib/proper_fractions.rb
+++ Proper_Fractions/lib/proper_fractions.rb
@@ -11,16 +11,29 @@ def proper_fractions(n)
 
 end
 
-def factors_of_n(n)
 
-  factors = []
+#ICIF the answer will just be the prime factors multiples less than n
+
+def prime_factors_of_n(n)
+
+  prime_factors = []
 
   for i in 1...n do
     if n % i == 0
-      factors.push(i)
+      prime_factors.push(i)
     end
   end
 
-  factors
+  prime_factors
+
+end
+
+def factors_less_than_n(prime_factors)
+
+  factors=[]
+
+  for i in prime_factors[i] do
+    
+  end
 
 end
