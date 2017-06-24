93facbd4e596601661ca90a159c325734d32941e
tmgree <tmgree@outlook.com>
2017-06-24 17:43:14 +0100

    fixed spacing

--- 7_kyu/Mumbling/lib/Mumbling.rb
+++ 7_kyu/Mumbling/lib/Mumbling.rb
@@ -1,5 +1,3 @@
 def accum(string)
-
-	string.chars.each_with_index.map { |c,i| "#{c*(i+1)}".capitalize }.join("-")
-	
+string.chars.each_with_index.map { |c,i| "#{c*(i+1)}".capitalize }.join("-")
 end
